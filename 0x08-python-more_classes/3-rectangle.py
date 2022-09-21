367bf7a1a3ed47f1cc1eaffd6d72f0b9f511e8e3
jemila ibrahim <jemiladotto@gmail.com>
2022-09-21 15:34:44 -0700

    task 3 redo

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-'''module: 3-rectangle
+"""module: 3-rectangle
 this module contains the class Rectangle ...
-'''
+"""
 
 
 class Rectangle:
@@ -44,16 +44,16 @@ class Rectangle:
         """returns the area of the rectangle"""
         return self.__width * self.__height
 
-     def perimeter(self):
-         """returns the perimeter of the rectangle"""
-         if self.__width == 0 or self.__height == 0:
-             return 0
-         return (self.__width * 2) + (self.__height * 2)
+    def perimeter(self):
+        """returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
 
-     def __str__(self):
-         """returns printable string representation of the rectangle"""
-         string = ""
-         if self.__width != 0 and self.__height != 0:
-             string += "\n".join("#" * self.__width
-                                 for j in range(self.__height))
-             return string
+    def __str__(self):
+        """returns printable string representation of the rectangle"""
+        string = ""
+        if self.__width != 0 and self.__height != 0:
+            string += "\n".join("#" * self.__width
+                    for j in range(self.__height))
+        return string
