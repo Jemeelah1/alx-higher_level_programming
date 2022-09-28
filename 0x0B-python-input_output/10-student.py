0f39bf6e6ee15cb0092db5dcd9be8a68609e74fb
jemila ibrahim <jemiladotto@gmail.com>
2022-09-27 19:40:24 -0700

    task 10 redo

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-'''module: 10-class_to_json
-'''
+"""module: 10-class_to_json
+"""
 
 
 def class_to_json(obj):
-    '''class_to_json
+    """class_to_json
     accepts: an object
     returns: builds a dictionary of attributes (no methods!) of the object
-    '''
+    """
 
     return obj.__dict__
